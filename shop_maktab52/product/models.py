1504775a6415691ca0f8e2286836aa6fc4c00d3a
AmirHossein <a.h.ansari.flv@gmail.com>
2021-08-08 17:42:02 +0430

    created product and category app and models

---
+++ shop_maktab52/product/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from core.models import BaseModel
+
+
+class Product(BaseModel):
+    Category = models.ForeignKey(
+        'Category', on_delete=models.CASCADE, related_name='Product'
+    )
+    Name = models.CharField(max_length=255)
+    Slug = models.SlugField(max_length=100, unique=True)
+    Images = models.ImageField(upload_to='Product/%y/%m/%d/%h/')
+    description = models.TextField()
+    Price = models.DecimalField(max_digits=12, decimal_places=3)
+    Available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.Name
+
+
+class Category(BaseModel):
+    Name = models.CharField(max_length=150)
+    Slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('Name',)
+        verbose_name = 'Category'
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.Name
