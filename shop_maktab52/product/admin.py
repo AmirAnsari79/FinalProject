43917808d6126e6ae37fd20ce4829df9f86d6dc2
AmirHossein <a.h.ansari.flv@gmail.com>
2021-08-09 10:48:51 +0430

    costume admin panel

--- shop_maktab52/product/admin.py
+++ shop_maktab52/product/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('Name',)
+    prepopulated_fields = {'Slug': ('Name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Price', 'Available',)
+    list_filter = ('Available',)
+    list_editable = ('Price', 'Available')
+    prepopulated_fields = {'Slug': ('Name', 'Price')}
+    raw_id_fields = ('Category',)
